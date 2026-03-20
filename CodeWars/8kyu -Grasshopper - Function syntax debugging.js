60b66fb89dd282309e19d1b37d9e5fbec53b578b
MartinMinggu <artminggu@gmail.com>
2026-03-20 20:49:49 +0700

    chalenge kedua dan ketiga

---
+++ CodeWars/8kyu -Grasshopper - Function syntax debugging.js
@@ -0,0 +1,43 @@
+/*
+Fix the function
+I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+
+// function 
+
+//js
+function main [verb, noun]
+  return verb + noun
+}
+*/
+// Javascript
+const main = (verb, noun) =>  verb + noun;
+console.log(main("test ", "saja"));
+
+
+// c#
+/*
+ function addFive(num) {
+  var total = num + 5
+  return num
+}
+*/
+// benar
+//   public static int AddFive(int num) => num + 5;
+
+
+
+// java
+/*
+public class Solution {
+  public static int addFive(int num) {
+    int total = num + 5;
+    return num;
+  }
+}
+
+*/
+
+
+
